78d53d808abc055194a4baa02d72a1f340fa0709
smatsutani <dear.old.days@gmail.com>
2026-06-30 12:48:14 +0900

    cleanup: 不要になったモバイル版検証画面およびルーティング定義の削除

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import MobileVerifier from './components/MobileVerifier';
-import './styles/light-theme.css';
+import React from 'react'
+import Dashboard from './components/Dashboard'
+import './styles/light-theme.css'
 
 export default function App() {
-    return (
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/mobile" element={<MobileVerifier />} />
-            </Routes>
-        </HashRouter>
-    );
+  return (
+    <Dashboard />
+  )
 }
